f2c92d30ce0f534980321a88490525e65da5cf56
Timothy Hoover <64495114+timothyhoover@users.noreply.github.com>
2022-12-08 16:29:44 -0500

    remove async

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,33 +1,8 @@
-import { unstable_getServerSession } from 'next-auth'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import LogoutButton from './LogoutButton'
 
-const Header = async () => {
-  const session = await unstable_getServerSession()
-  console.log(session)
-  if (session)
-    return (
-      <header className="sticky top-0 z-50 bg-white flex justify-between items-center p-10 shadow-md">
-        <div className="flex space-x-2">
-          <Image
-            className="rounded-full mx-2 object-contain"
-            src={session.user?.image!}
-            alt="Logo"
-            height={10}
-            width={50}
-          />
-
-          <div>
-            <p className="text-blue-400">Logged in as:</p>
-            <p className="font-bold text-lg">{session.user?.name}</p>
-          </div>
-        </div>
-        <LogoutButton />
-      </header>
-    )
-
+const Header = () => {
   return (
     <header className="sticky top-0 z-50 bg-white flex justify-center items-center p-10 shadow-md">
       <div className="flex flex-col items-center space-y-5">
